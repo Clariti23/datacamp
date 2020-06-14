f42a8a8ce7e7bfa6e38ac33dd2ead7dd2d621b79
Farmwell <g.ferowich@gmail.com>
2020-06-13 21:02:37 -0500

    clean honorifics

--- cleaningTextData.py
+++ cleaningTextData.py
@@ -3,3 +3,17 @@
 phones['Phone number'] = phones['Phone number'].str.replace("+", "00")
 # Replace dashes (-) in phone number column of phones with ""
 phones['Phone number'] = phones['Phone number'].str.replace("-", "")
+
+# Clean honorifics
+# Replace "Dr." with empty string ""
+airlines['full_name'] = airlines['full_name'].str.replace("Dr.", "")
+
+# Replace "Mr." with empty string ""
+airlines['full_name'] = airlines['full_name'].str.replace("Mr.", "")
+
+# Replace "Miss" with empty string ""
+airlines['full_name'] = airlines['full_name'].str.replace("Miss", "")
+# Replace "Ms." with empty string ""
+airlines['full_name'] = airlines['full_name'].str.replace("Ms.", "")
+# Assert that full_name has no honorifics
+assert airlines['full_name'].str.contains('Ms.|Mr.|Miss|Dr.').any() == False
