9391abeb5e7141525fb95cf165447d7668f4ed2b
Farmwell <g.ferowich@gmail.com>
2020-06-13 21:50:35 -0500

    treating nonuniform data

--- cleaningTextData.py
+++ cleaningTextData.py
@@ -17,3 +17,24 @@ airlines['full_name'] = airlines['full_name'].str.replace("Miss", "")
 airlines['full_name'] = airlines['full_name'].str.replace("Ms.", "")
 # Assert that full_name has no honorifics
 assert airlines['full_name'].str.contains('Ms.|Mr.|Miss|Dr.').any() == False
+
+# Store length of each row in survey_response column
+resp_length = airlines['survey_response'].str.len()
+
+# Find rows in airlines where resp_length > 40
+airlines_survey = airlines[resp_length > 40]
+
+# Assert minimum survey_response length is > 40
+assert airlines_survey['survey_response'].str.len().min() > 40
+
+# Print new survey_response column
+print(airlines_survey['survey_response'])
+
+# Isolate temperatures in farenheit,
+
+temp_fahrenheit = temperatures.loc[temperatures['Temperature']
+                                   > 40, 'Temperature']
+
+#  When you want to convert dates in a column into a single format
+
+birthdays['Birthday'] = pd.to_datetime[birthdays['Birthday'], infer_datetime_format = True, errors = 'coerce']
